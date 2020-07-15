5e0d175d5a58ee51ce3126988a92b3e7d1c2df6d
hss-mateus <hss-mateus@tuta.io>
2020-07-15 10:54:08 -0300

    Add Typeclasses

---
+++ Typeclasses.hs
@@ -0,0 +1,74 @@
+module Typeclasses where
+
+data Trivial = Trivial'
+
+instance Eq Trivial where
+  Trivial' == Trivial' = True
+
+data DayOfWeek =
+  Mon | Tue | Wed | Thu | Fri | Sat | Sun
+
+instance Eq DayOfWeek where
+  (==) Mon Mon   = True
+  (==) Tue Tue   = True
+  (==) Wed Wed   = True
+  (==) Thu Thu   = True
+  (==) Fri Fri   = True
+  (==) Sat Sat   = True
+  (==) Sun Sun   = True
+  (==) _ _       = False
+
+data Date =
+  Date DayOfWeek Int
+
+instance Eq Date where
+  (==) (Date weekday dayOfMonth) (Date weekday' dayOfMonth') =
+    weekday == weekday' && dayOfMonth == dayOfMonth'
+
+data TisAnInteger = TisAn Integer
+
+instance Eq TisAnInteger where
+  (==) (TisAn x) (TisAn y) = x == y
+
+data TwoIntegers = Two Integer Integer
+
+instance Eq TwoIntegers where
+  (==) (Two x y) (Two a b) = x == a && y == b
+
+data StringOrInt = TisAnInt Int | TisAString String
+
+instance Eq StringOrInt where
+  (==) (TisAnInt x) (TisAnInt y) = x == y
+  (==) (TisAString x) (TisAString y) = x == y
+  (==) _ _ = False
+
+data Pair a = Pair a a
+
+instance Eq a => Eq (Pair a) where
+  (==) (Pair x y) (Pair a b) = x == a && y == b
+
+data Tuple a b = Tuple a b
+
+instance (Eq a, Eq b) => Eq (Tuple a b) where
+  (==) (Tuple x y) (Tuple p q) = x == p && y == q
+
+data Which a = ThisOne a | ThatOne a
+
+instance Eq a => Eq (Which a) where
+  (==) (ThisOne x) (ThisOne y) = x == y
+  (==) (ThatOne x) (ThatOne y) = x == y
+  (==) (ThisOne x) (ThatOne y) = x == y
+  (==) (ThatOne x) (ThisOne y) = x == y
+
+data EitherOr a b = Hello a | Goodbye b
+
+instance (Eq a, Eq b) => Eq (EitherOr a b) where
+  (==) (Hello x) (Hello y) = x == y
+  (==) (Goodbye x) (Goodbye y) = x == y
+  (==) _ _ = False
+
+instance Ord DayOfWeek where
+  compare Fri Fri = EQ
+  compare Fri _   = GT
+  compare _ Fri   = LT
+  compare _ _     = EQ
